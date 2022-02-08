53b1c87af2c1e681634c9b635f9f7e1c316e2208
Litchi Pi <litchi.pi@protonmail.com>
2022-02-08 15:54:41 +0100

    lib: data: Adding copy_dirs_to_home functions set
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- lib/manage_data.nix
+++ lib/manage_data.nix
@@ -1,4 +1,19 @@
 { config, lib, pkgs, ... }:
+let
+  mergeall = setlist: lib.lists.fold (set: acc: lib.attrsets.recursiveUpdate acc set) {} setlist;
+  list_elements = dir: type: map (f: dir + "/${f}") (
+    lib.attrNames (
+      lib.filterAttrs
+        (_: entryType: entryType == type)
+        (builtins.readDir  dir)
+      )
+    );
+
+  find_all_files = dir: lib.lists.flatten (
+    (builtins.map find_all_files (list_elements dir "directory"))
+    ++ (list_elements dir "regular")
+  );
+in
 rec {
   get_data_path = pathlist:
     builtins.foldl' (p: d: p + "/${d}") ../data pathlist;
@@ -23,4 +38,33 @@ rec {
   load_wifi_cfg = ssid: pwds.wifi_keys."${ssid}" or null;
 
   get_asset_path = ident: get_data_path [ "asset" ident ];
+
+  copy_files_in_home = assets:
+    builtins.listToAttrs (
+      builtins.map (asset:
+        { name = asset.home_path; value = { source = get_data_path asset.asset_path; }; }
+      ) assets
+    );
+
+  copy_dir_to_home = home_path_dir: dir_path: let
+    data_dir_path = get_data_path dir_path;
+    all_dirs = list_elements data_dir_path "directory";
+    all_files = list_elements data_dir_path "regular";
+  in
+    copy_files_in_home (
+      builtins.map (f: {
+        home_path = home_path_dir + "/${builtins.baseNameOf f}";
+        asset_path = dir_path ++ [ (builtins.baseNameOf f) ];
+      }) all_files
+    ) // copy_dirs_to_home (
+      builtins.map (d: {
+        home_path_dir = home_path_dir + "/${builtins.baseNameOf d}";
+        asset_path_dir = dir_path ++ [ (builtins.baseNameOf d) ];
+      }) all_dirs
+    );
+
+  copy_dirs_to_home = dirs:
+    mergeall (lib.lists.flatten (
+      builtins.map (d: copy_dir_to_home d.home_path_dir d.asset_path_dir) dirs
+    ));
 }
