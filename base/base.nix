3d9d52fb796162fcf548e9f486b17abab9b7e1bd
Litchi Pi <litchi.pi@protonmail.com>
2022-03-01 17:01:00 +0100

    base: Setting up PS1 base configuration, using git-prompt for it
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- base/base.nix
+++ base/base.nix
@@ -4,6 +4,7 @@ let
   libdata = import ../lib/manage_data.nix {inherit config lib pkgs;};
   libssh = import ../lib/ssh.nix {inherit config lib pkgs;};
   libutils = import ../lib/utils.nix {inherit config lib pkgs;};
+  colors = import ../lib/colors.nix {inherit config lib pkgs;};
 
   extract_home_conf = conf: if (lib.attrsets.hasAttr "home_conf" conf)
     then conf.home_conf
@@ -36,6 +37,10 @@ let
       userName = libutils.email_to_name cfg.email;
       userEmail = cfg.email;
     };
+    programs.bash.initExtra = ''
+      source ${libdata.get_data_path [ "shell" "git-prompt.sh" ]}
+      export PS1="${colors.fg.ps1.username}\\u ${colors.fg.ps1.wdir}\\w ${colors.fg.ps1.gitps1}\`__git_ps1 \<%s\>\` ${colors.fg.ps1.dollarsign}$ ${colors.reset}";
+    '';
   };
 in
 {
