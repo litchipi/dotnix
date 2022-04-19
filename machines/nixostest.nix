217de4180782b57c570343b59817d9e57981eacd
Litchi Pi <litchi.pi@protonmail.com>
2022-04-19 17:01:24 +0400

    machines: nixostest: Modifying SSH auth keys to be set
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- machines/nixostest.nix
+++ machines/nixostest.nix
@@ -8,7 +8,7 @@
 
   # What SSH key to allow for remote login
   #   (has to be a file in data/ssh/pubkeys/<name>.pub)
-  base.networking.ssh_auth_keys = ["john"];
+  base.networking.ssh_auth_keys = ["john" "tim"];
 
   # Load the wifi password for the SSID "nixostest"
   base.networking.connect_wifi = [ "nixostest" ];
