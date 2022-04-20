8f8a53486ac7d6d6a079296fcf302f5d32ea5772
Litchi Pi <litchi.pi@protonmail.com>
2022-04-20 10:22:32 +0400

    fixup: Fix unknown bug in lib/manage_data
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- lib/manage_data.nix
+++ lib/manage_data.nix
@@ -16,9 +16,8 @@ let
   );
 in
   rec {
-  # TODO Throws an error when parenthesis are taken off ?
   get_data_path = pathlist:
-    (builtins.foldl' (p: d: p + "/${d}") ../data pathlist);
+    builtins.foldl' (p: d: p + "/${d}") ../data pathlist;
   read_data = pathlist: builtins.readFile (get_data_path pathlist);
 
   read_data_else_empty = pathlist:
