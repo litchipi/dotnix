82fd56075782d60f6531ad4b2d5e77b683742feb
Litchi Pi <litchi.pi@protonmail.com>
2022-04-29 09:33:41 +0400

    machines: company_server: clean code, enable gitlab, set nextcloud theme
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- machines/company_server.nix
+++ machines/company_server.nix
@@ -3,37 +3,40 @@ let
   company_name="tyf";
 in
 {
-  # The name of the main user of the system
   base.user = "op";
-
-  # The system hostname
   base.hostname = "${company_name}";
-
-  # What SSH key to allow for remote login
-  #   (has to be a file in data/ssh/pubkeys/<name>.pub)
   base.networking.ssh_auth_keys = [ "john" "tim" ];
 
   cmn.server.enable = true;
   # TODO  Investigate 502 timeout error
-  # cmn.services.gitlab.enable = true;
+  cmn.services.gitlab.enable = true;
 
   cmn.services.nextcloud = {
     enable = true;
 
-    # TODO Pimp theming from here
     theme = {
       name = company_name;
-      logo = ../data/assets/tyf/logo.png;
+      logo = ../data/assets/nextcloud/${company_name}/logo.svg;
+      favicon = ../data/assets/nextcloud/${company_name}/logo.svg;
+      logoheader = ../data/assets/nextcloud/${company_name}/logo.svg;
+      background = ../data/assets/nextcloud/${company_name}/background.jpg;
       slogan = "Where beer really matters";
-      color = "#9A2462";
-      background = ../data/assets/tyf/background.jpg;
-      favicon = ../data/assets/tyf/favicon.png;
+      color = "#6E1852";
     };
+
+    disable_apps = [
+      "photos"
+    ];
   };
 
+  # TODO Create a Jitsi-meet service and configure Nextcloud to use it
+  # https://search.nixos.org/options?channel=21.11&from=0&size=50&sort=relevance&type=packages&query=jitsi-meet
+
+  # TODO  Add protonmail-bridge to server and use it to serve Nextcloud Mail
+
+  # TODO Set up restic backup
   # Additionnal packages to install
   base.add_pkgs = with pkgs; [
-    wget
     cowsay
   ];
 }
