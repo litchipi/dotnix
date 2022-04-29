f38bb18239fc2171bf2878229f98e679363de22e
Litchi Pi <litchi.pi@protonmail.com>
2022-04-29 09:34:17 +0400

    script: generate_provision_key: Create script for provision key generation
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

---
+++ generate_provision_key.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 hostname"
+    exit 1;
+fi
+
+HOST=$1
+cd data/secrets/provision_key/
+rm -f $HOST.pub $HOST.gpg
+ssh-keygen -P "" -f $HOST -t ed25519
+gpg -q -c --passphrase-file ./provision_key_password --batch $HOST
+srm $HOST
+
