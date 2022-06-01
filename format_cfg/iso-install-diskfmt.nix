caf04d8e8718cae4a351795a5acfc3eb66497a9b
Litchi Pi <litchi.pi@protonmail.com>
2022-06-01 09:23:31 +0200

    format_cfg: Improve and fix diskfmt script
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- format_cfg/iso-install-diskfmt.nix
+++ format_cfg/iso-install-diskfmt.nix
@@ -9,6 +9,7 @@ in
   config = let
     get_addpart_size = part: ((part.size.Gib or 0) * 1024) + (part.size.Mib or 0);
     add_parts_sz = builtins.foldl' (acc: sz: acc + sz) 0 (builtins.map get_addpart_size cfg.add_partition);
+    root_end = -(add_parts_sz+(cfg.swapsize*1024)+2);
 
     disp_disks = ''lsblk|grep disk --color=none|'' +
       ''awk '{print "\tDevice ${colors.fg.primary}"$1"${colors.reset} '' +
@@ -27,6 +28,9 @@ in
           echo -e "${colors.fg.tertiary}$target${colors.reset} is ${colors.fg.bad}not a valid${colors.reset} disk"
         fi
       done
+
+      echo -n "Enter the suffix added to the dev for each partition (leave blank if none): "
+      read suffix
       
       target=/dev/$target
 
@@ -34,24 +38,29 @@ in
       FAT_MKFS="msdos fat vfat"
 
       NB_PART=0
-    '' + (if cfg.use_uefi then build_script_uefi else build_script_legacy));
-
-    root_end = -(add_parts_sz+(cfg.swapsize*1024)+2);
+    '' + (if cfg.use_uefi then build_script_uefi else build_script_legacy) + ''
+        echo -e -n "${colors.fg.primary}Finished successfully${colors.reset}, "
+        echo -e "you can now install the NixOS system using the command ${colors.fg.secondary}install_nixos${colors.reset}"
+    '');
 
     build_script_uefi = (
       create_table "gpt" +
       (
         if cfg.disk_encryption then
-          (create_encrypted_lvm_partition { start=512; end=-add_parts_sz; })
+          (create_encrypted_lvm_partition { start=512; end=-(add_parts_sz + 1); })
         else (
           create_partition cfg.root_part_label 512 root_end "ext4"
           create_partition "swap" root_end (-(add_parts_sz+1)) "linux-swap"
         )
       ) + ''
+        echo -e "${colors.fg.secondary}Creating ESP partition${colors.reset}"
+        echo -e "${colors.fg.tertiary}======================================================${colors.reset}"
         NB_PART=$((NB_PART+1))
         parted $target -- mkpart ESP fat32 1MiB 512MiB
-        parted $target -- set 3 esp on
-        mkfs.fat -F 32 -n boot $target$NB_PART
+        parted $target -- set 2 esp on
+        mkfs.fat -F 32 -n boot $target$suffix$NB_PART
+        echo -e "${colors.fg.secondary}Creating additionnal partition${colors.reset}"
+        echo -e "${colors.fg.tertiary}======================================================${colors.reset}"
       '' + (create_add_parts (-(add_parts_sz+1)))
     );
 
@@ -59,7 +68,7 @@ in
       create_table "msdos" +
       (
         if cfg.disk_encryption then
-          (create_encrypted_lvm_partition { start=1; end=-add_parts_sz; })
+          (create_encrypted_lvm_partition { start=1; end=-(add_parts_sz + 1); })
         else (
           create_partition cfg.root_part_label 1 root_end "ext4"
           create_partition "swap" root_end (-(add_parts_sz+1)) "linux-swap"
@@ -86,24 +95,26 @@ in
 
       parted $target -- mkpart primary ${fstype} ${start} ${end}
       if [[ "$UNIX_MKFS" == *"${fstype}"* ]]; then
-        mkfs.${fstype} -L ${label} $target$NB_PART
+        mkfs.${fstype} -L ${label} $target$suffix$NB_PART
       elif [[ "$FAT_MKFS" == *"${fstype}"* ]]; then
-        mkfs.${fstype} -F 32 -n ${label} $target$NB_PART
+        mkfs.${fstype} -F 32 -n ${label} $target$suffix$NB_PART
       elif [[ "${fstype}" == "linux-swap" ]]; then
-        mkswap -L ${label} $target$NB_PART
-        swapon $target$NB_PART
+        mkswap -L ${label} $target$suffix$NB_PART
       fi
     '';
 
-    #TODO Encryption
-    create_encrypted_lvm_partition = { start, end }: ''
+    create_encrypted_lvm_partition = { start, end }: let
+      keyfile = config.base.secrets.store.luks_encryption.dest;
+    in ''
       echo -e "${colors.fg.secondary}Encrypted LVM partitions${colors.reset}"
       echo -e "${colors.fg.tertiary}======================================================${colors.reset}"
 
+      echo "Start: ${builtins.toString start}, end: ${builtins.toString end}"
+
       NB_PART=$((NB_PART+1))
-      parted $target$NB_PART -- mkpart primary ${builtins.toString start} ${builtins.toString end}
-      cryptsetup luksFormat $target$NB_PART
-      cryptsetup luksOpen $target$NB_PART encpart
+      parted $target -- mkpart primary ${builtins.toString start} ${builtins.toString end}
+      cryptsetup luksFormat -d ${keyfile} $target$suffix$NB_PART
+      cryptsetup luksOpen -d ${keyfile} $target$suffix$NB_PART encpart
 
       pvcreate /dev/mapper/encpart
       vgcreate vg /dev/mapper/encpart
@@ -126,13 +137,7 @@ in
     ).script;
   in
   {
-    boot.postBootCommands = ''
-      set -e
-      echo "POSTBOOTCOMMAND"
-      diskfmt
-      install_nixos
-      set +e
-    '';
+    base.secrets.store.luks_encryption = libdata.set_secret "root" ["disk_encryption" config.base.hostname] {};
     environment.systemPackages = [
       pkgs.cryptsetup
       pkgs.lvm2
