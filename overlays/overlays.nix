d236c2b9ce2e0708c6717667fb016563cdb7255b
Litchi Pi <litchi.pi@proton.me>
2022-07-14 16:03:39 +0200

    overlays: Add youtube-dl from github repository
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- overlays/overlays.nix
+++ overlays/overlays.nix
@@ -13,4 +13,18 @@ in
       name = package;
       value = (pkgs.callPackage (./. + "/${package}/default.nix") {});
     }) (list_dirs ./.));
+
+    youtube-dl = pkgs.stdenv.mkDerivation {
+      name = "youtube-dl";
+      src = pkgs.fetchurl {
+        url = "https://yt-dl.org/downloads/latest/youtube-dl";
+        sha256 = "sha256-eIDgGr4oLH/VlvQpw1GJhRGA1hdzArshW+HN7HjW0G0=";
+      };
+      phases = "installPhase";
+      installPhase = ''
+        mkdir -p $out/bin
+        cp $src $out/bin/youtube-dl
+        chmod +x $out/bin/youtube-dl
+      '';
+    };
   }
