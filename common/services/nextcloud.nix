1a0dbae984a3b15e62055d86f3ef3ae899267e9b
Litchi Pi <litchi.pi@protonmail.com>
2022-08-26 10:14:14 +0200

    cmn: services: nextcloud: Update apps, pin package, improve config
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- common/services/nextcloud.nix
+++ common/services/nextcloud.nix
@@ -118,15 +118,22 @@ libconf.create_common_confs ([
         nextcloud_adminpass = nextcloud_secret "adminpass";
       };
 
+      cmn.services.postgresql.users.nextcloud = {
+        databases = [ "nextcloud" ];
+        permissions."nextcloud" = "ALL PRIVILEGES";
+        auth_method = "trust";
+      };
+
       services.nextcloud = {
         enable = true;
+        package = pkgs.nextcloud24;
         hostName = "nextcloud.${config.base.networking.domain}";
         home = "/var/nextcloud/";
         appstoreEnable = true;
         config = {
           dbtype = "pgsql";
           dbuser = "nextcloud";
-          dbhost = "/run/postgresql";
+          dbhost = "/run/postgresql"; #config.services.postgresql.dataDir;
           dbname = "nextcloud";
           adminpassFile = config.base.secrets.store.nextcloud_adminpass.dest;
           adminuser = "root";
@@ -171,9 +178,9 @@ libconf.create_common_confs ([
   }
 ] ++ (nextcloud_apps {
   breezedark = {
-    sha256 = "sha256-id0eZrkXe4PQchEfgT79HJtIuu/xaoTCNlw8XT34zZ8=";
-    url = "https://github.com/mwalbeck/nextcloud-breeze-dark/releases/download/v23.2.1/breezedark.tar.gz";
-    version = "23.2.1";
+    sha256 = "sha256-NHgeCqnOrwtLuxXWSZ4ThBRkQHZmbya5DVfYRolztG8=";
+    url = "https://github.com/mwalbeck/nextcloud-breeze-dark/releases/download/v24.0.2/breezedark.tar.gz";
+    version = "24.0.2";
   };
 
   files_readmemd = {
@@ -186,12 +193,6 @@ libconf.create_common_confs ([
     sha256 = "sha256-469nRFdKIW0e+CO1Py5l/guTZ2dhH+F1cknJZ4Pt3gg=";
     url = "https://github.com/nextcloud-releases/mail/releases/download/v1.11.7/mail-v1.11.7.tar.gz";
     version = "1.11.7";
-  };
-
-  spreed = {
-    sha256 = "sha256-566oKMVRBjaZ69Ntg4zC7rjW01u0GF4RT4vJnzIl2Zk=";
-    url = "https://github.com/nextcloud-releases/spreed/releases/download/v13.0.5/spreed-v13.0.5.tar.gz";
-    version = "13.0.5";
   };
 
   tasks = {
@@ -219,9 +220,9 @@ libconf.create_common_confs ([
   };
 
   twofactor_totp = {
-    sha256 = "sha256-r6WuXAvGXIEn6SViBxyMp98JBTWL6fR8MJDcdba1gA8=";
-    url = "https://github.com/nextcloud-releases/twofactor_totp/releases/download/v6.2.0/twofactor_totp.tar.gz";
-    version = "6.2.0";
+    sha256 = "sha256-cRtpRs1s31l8xG84YkZIuR3C3pg2kQFNlrY2f5NTSBo=";
+    url = "https://github.com/nextcloud-releases/twofactor_totp/releases/download/v6.4.0/twofactor_totp-v6.4.0.tar.gz";
+    version = "6.4.0";
   };
 
   unsplash = {
@@ -231,28 +232,10 @@ libconf.create_common_confs ([
     version = "1.2.4";
   };
 
-  drawio = {
-    sha256 = "sha256-9UM3rXD4gqzx0DRPHf5Rpa6Bz/iG8muDPmn4YYdKRIQ=";
-    url = "https://github.com/pawelrojek/nextcloud-drawio/releases/download/v.1.0.2/drawio-v1.0.2.tar.gz";
-    version = "1.0.2";
-  };
-
-  apporder = {
-    sha256 = "sha256-p3VWxTYDCO2NePq6oLM8tBVqYkvoB7itqxp7IZwGDnE=";
-    url = "https://github.com/juliushaertl/apporder/releases/download/v0.15.0/apporder.tar.gz";
-    version = "0.15.0";
-  };
-
   files_downloadactivity = {
     sha256 = "sha256-JMJM0GL5zpaNUHIGl1J37JdZlhrdL0TBXD9++bB6nvM=";
     url = "https://github.com/nextcloud-releases/files_downloadactivity/releases/download/v1.13.0/files_downloadactivity-v1.13.0.tar.gz";
     version = "1.13.0";
-  };
-
-  epubreader = {
-    sha256 = "sha256-J0iW+WiCY0XEALbECE7lChnUslIOcqSCKFcDa3ZCuj0=";
-    url = "https://github.com/e-alfred/epubreader/releases/download/1.4.7/epubreader-1.4.7.tar.gz";
-    version = "1.4.7";
   };
 
   integration_gitlab = {
@@ -261,18 +244,13 @@ libconf.create_common_confs ([
     version = "1.0.3";
   };
 
+  # TODO FIXME
   # jitsi = {
   #   sha256 = "sha256-aFwYQpb2WrPD00qPtCu5zGu0LlKFYdwZYTAmvX2li4o=";
   #   url = "https://pubcode.weimann.digital/downloads/projects/nextcloud-jitsi/builds/48/artifacts/nextcloud-jitsi.tar.gz";
   #   version = "0.15.0";
   # };
 
-  # appointments = {
-  #   sha256 = "sha256-gnmUGZDYtPh+Z1dCEyEue0Cqzs1dA8teBZi3rIiZmBw=";
-  #   url = "https://github.com/SergeyMosin/Appointments/raw/95c9fcb8ef495a032ac5f1ea58dfb47fca871e55/build/artifacts/appstore/appointments.tar.gz";
-  #   version = "1.12.3";
-  # };
-  #
   approval = {
     sha256 = "sha256-SkkmWJbCSuJGav5BBP7yzvu1oV3r1UvNQQYyUEoDQXg=";
     url = "https://github.com/nextcloud/approval/releases/download/v1.0.9/approval-1.0.9.tar.gz";
@@ -280,9 +258,9 @@ libconf.create_common_confs ([
   };
 
   external = {
-    sha256 = "sha256-nenSP0Ou8DXoL4el3xS2xjgCwuMG2yWzP/qIowJqrBU=";
-    url = "https://github.com/nextcloud-releases/external/releases/download/v3.10.2/external-v3.10.2.tar.gz";
-    version = "3.10.2";
+    sha256 = "sha256-iDROokpqcxZY4DnNRO+BYsCJ55OMabQ+RrS7nOQOBlU=";
+    url = "https://github.com/nextcloud-releases/external/releases/download/v4.0.0/external.tar.gz";
+    version = "4.0.0";
   };
 
   polls = {
@@ -300,15 +278,15 @@ libconf.create_common_confs ([
   riotchat = {
   # Enabled in the configuration of the synapse service
     enable = false;
-    sha256 = "sha256-BgemR0c9oFk2vGm5h17vpqhsHGneHEswBnwOpD98qSc=";
-    url = "https://github.com/gary-kim/riotchat/releases/download/v0.11.3/riotchat.tar.gz";
-    version = "0.11.3";
+    sha256 = "sha256-hL1FbP9ZchGwqPJ1U8fohy2aKAu7D7Ojq5a7voqDqJA=";
+    url = "https://github.com/gary-kim/riotchat/releases/download/v0.13.5/riotchat.tar.gz";
+    version = "0.13.5";
   };
 
   deck = {
-    sha256 = "sha256-Ze20arex7AZhHnSWIPN8DlAtalp+/Rl7qwTA+IBv3vo=";
-    url = "https://github.com/nextcloud-releases/deck/releases/download/v1.6.1/deck-v1.6.1.tar.gz";
-    version = "1.6.1";
+    sha256 = "sha256-G4v1B5XHYuKEZxNhkd7Fu5OSbzwcS7yFaDkUkydpdPU=";
+    url = "https://github.com/nextcloud-releases/deck/releases/download/v1.7.1/deck-v1.7.1.tar.gz";
+    version = "1.7.1";
   };
 
   gestion = {
