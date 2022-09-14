001c7cada308aaf844ad3480a50c8d0a76487cc8
Litchi Pi <litchi.pi@protonmail.com>
2022-09-14 19:24:59 +0200

    hardware: suzie: Add new hardware configuration
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- hardware/suzie.nix
+++ hardware/suzie.nix
@@ -2,46 +2,42 @@
   libdata = import ../lib/manage_data.nix { inherit config pkgs lib; };
 in {
   base.hostname = "suzie";
-  base.disks.add_swapfile = 12000;
-  boot.kernel.sysctl = { "vm.swappiness" = 30;};
-  base.secrets.store."suzie_storage_keyfile" = libdata.set_secret config.base.user [ "keys" "suzie" "storage" ] { group = "users"; };
 
+  # Hardware configuration auto-detected
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/dc53b99d-2462-4c02-ad17-125306576d24";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/3a7abaa7-7754-43e5-aceb-4e16cca1677a";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/1107-5D03";
-      fsType = "vfat";
-    };
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-uuid/1958-1F6F";
+    fsType = "vfat";
+  };
 
-  # TODO Setup encrypted storage for suzie      -> In software config ?
-  # fileSystems."/storage" = {
-  #     encrypted = {
-  #       enable = true;
-  #       blkDev = "/dev/disk/by-uuid/ad3410d0-318b-4873-bc8d-383338303fdb";
-  #       keyFile = config.base.secrets.store."suzie_storage_keyfile".dest;
-  #     };
-  #     fsType = "ext4";
-  #     options = ["auto" "noexec" "nosuid" "rw" "nouser" "sync" ];
-  #   };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/99ac1e2d-7ed7-4aeb-9ab1-752273452da4";
+    fsType = "btrfs";
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/b1b95521-ba1f-4b55-8fbd-4ae25f2bf288"; }
-    ];
+  fileSystems."/var" = {
+    device = "/dev/disk/by-uuid/575ed9ce-c3f8-457a-a08e-8002c03d45fb";
+    fsType = "btrfs";
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/dd3c0f8b-18cc-4b45-ad6f-f5283abdce56"; }
+  ];
 
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s20u6.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
