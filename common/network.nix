8fdfba85066d2804deb411ebaf670ea1c7d9d3ce
Litchi Pi <litchi.pi@proton.me>
2022-10-22 10:33:20 +0200

    cmn: network: Improve protonvpn-cli config & aliases
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- common/network.nix
+++ common/network.nix
@@ -24,38 +24,39 @@ conf_lib.create_common_confs [
 
     add_pkgs = with pkgs; [
       protonvpn-cli
-      expect
     ];
 
-  home_cfg.home.file.".protonvpn_login.tcl".text = ''
-      set user [lindex $argv 0]
-      set p [lindex $argv 1]
-      spawn -noecho protonvpn-cli login $user
-      expect {
-        "already logged" { exit 0 };
-        "password" {
-            send_user "Entering password";
-            send "$p\n";
-            interact;
-            exit 0;
+    cfg.base.secrets.store.protonvpn_creds = libdata.set_secret {
+      user = config.base.user;
+      path = ["keys" config.base.hostname "protonvpn_creds" ];
+    };
+    cfg.environment.shellAliases = let
+      protonvpn_login = pkgs.writeText "protonvpn_login.tcl" ''
+        set user [lindex $argv 0]
+        set p [lindex $argv 1]
+        spawn -noecho protonvpn-cli login $user
+        expect {
+          "already logged" { exit 0 };
+          "password" {
+              send_user "Entering password";
+              send "$p\n";
+              interact;
+              exit 0;
+          };
+          timeout { send_user "Timeout"; exit 254 };
+          eof { exit 253 };
         };
-        timeout { send_user "Timeout"; exit 254 };
-        eof { exit 253 };
-      };
-    '';
-    home_cfg.programs.bash = {
-      enable = true;
-      shellAliases = {
-        vpn_logout = "protonvpn-cli logout";
-        vpn_login = "expect $HOME/.protonvpn_login.tcl ${vpncfg.username} $(cat ${vpncreds})";
-        vpn = "protonvpn-cli c --sc";
-        vpn_p2p = "protonvpn-cli c --p2p";
-        vpn_tor = "protonvpn-cli c --tor";
-        vpn_cc = "protonvpn-cli c --cc";
-        vpn_random = "protonvpn-cli c -r";
-        vpn_fast = "protonvpn-cli c -f";
-        novpn = "protonvpn-cli d";
-      };
+      '';
+    in {
+      vpn_logout = "protonvpn-cli logout";
+      vpn_login = "${pkgs.expect}/bin/expect ${protonvpn_login} ${vpncfg.username} $(cat ${config.base.secrets.store.protonvpn_creds.dest})";
+      vpn = "protonvpn-cli c --sc";
+      vpn_p2p = "protonvpn-cli c --p2p";
+      vpn_tor = "protonvpn-cli c --tor";
+      vpn_cc = "protonvpn-cli c --cc";
+      vpn_random = "protonvpn-cli c -r";
+      vpn_fast = "protonvpn-cli c -f";
+      novpn = "protonvpn-cli d";
     };
   }
 ]
