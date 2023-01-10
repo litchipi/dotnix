0c911f30f4b6c1c9a86264c77b4575d4d359866a
Litchi Pi <litchi.pi@proton.me>
2023-01-10 07:59:58 +0100

    ci: cli: services: Add tests on shiori config and restic.global backup
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- ci/cli/services.nix
+++ ci/cli/services.nix
@@ -1,7 +1,7 @@
 machines: { ... }: {
-  cmn.services.gitlab = machines.suzie.cmn.services.gitlab // {
-    backup.gdrive = false;
-  };
+  cmn.services.restic.global = machines.suzie.cmn.services.restic.global;
+  cmn.services.gitlab = machines.suzie.cmn.services.gitlab;
+  cmn.services.shiori = machines.suzie.cmn.services.shiori;
   cmn.services.paperless = machines.suzie.cmn.services.paperless;
   cmn.services.conduit.enable = machines.suzie.cmn.services.conduit.enable;
   cmn.services.nextcloud = machines.suzie.cmn.services.nextcloud // {
