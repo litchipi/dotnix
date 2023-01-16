4da495a90f174b786dd082912b5745fa20634452
Litchi Pi <litchi.pi@proton.me>
2023-01-16 20:10:18 +0100

    set up dns to debug
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- common/services/dns.nix
+++ common/services/dns.nix
@@ -28,7 +28,7 @@ libconf.create_common_confs [
       enable = true;
       settings = {
         httpPort = cfg.blocky.metrics_port;
-        logLevel = "info";
+        logLevel = "debug";
         upstream.default = config.networking.nameservers;
         blocking.blackLists.custom = [
           "${inputs.StevenBlackHosts}/alternates/fakenews-gambling/hosts"
