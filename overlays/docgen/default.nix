b5faab9c6d0589293e8894a88ecbd1adba851764
Litchi Pi <litchi.pi@proton.me>
2024-01-04 17:59:17 +0100

    add docgen
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

---
+++ overlays/docgen/default.nix
@@ -0,0 +1,18 @@
+{lib, rustPlatform, fetchFromGitHub }: rustPlatform.buildRustPackage rec {
+  pname = "docgen";
+  version = "0.1.0";
+  src = fetchFromGitHub {
+    owner = "litchipi";
+    repo = "docgen";
+    rev = "f957832110b0647a8c7253fac6005ed328c29000";
+    sha256 = lib.fakeSha256;
+  };
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+    outputHashes = {
+      "comemo-0.3.1" = "sha256-myXQsrPx4NwIWli8VyKFbjUQaLhPXQA0achLO8g95Sg=";
+      "svg2pdf-0.9.1" = "sha256-B54OZ/xuiE3q+kMwrfyDOLP6iEUpQL/aOexRKq1EvI8=";
+      "typst-0.10.0" = "sha256-nSvYc/JUhDZGHFot7/WhhRbOBgNT+ZTVtKG71vY7raA=";
+    };
+  };
+}
