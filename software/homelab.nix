77fc5073ee3302cea87d37e0e202252ed635cefd
Litchi Pi <litchi.pi@proton.me>
2024-07-18 15:44:39 +0200

    Setup fail2ban for suzie
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- software/homelab.nix
+++ software/homelab.nix
@@ -33,6 +33,12 @@
     })
   ];
 
+  services.fail2ban = {
+    enable = true;
+    ignoreIP = [ "192.168.0.0/16" ];
+    bantime = "6h";
+  };
+
   services.paperless = {
     enable = true;
     package = pkgs_unstable.paperless-ngx;
